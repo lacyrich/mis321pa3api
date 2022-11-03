19c0550048fdf8b472b5744fd4d6ff8b811baf90
Lacy <44073703+lacyrich@users.noreply.github.com>
2022-11-03 11:20:36 -0500

    hi

--- api/DataAccess/DriverDataAccess.c.cs
+++ api/DataAccess/DriverDataAccess.c.cs
@@ -20,21 +20,13 @@ namespace mis321pa3api.api.DataAccess
             MySqlCommand cmd = new MySqlCommand(stm, con);
             MySqlDataReader rdr = cmd.ExecuteReader();
 
-            while(rdr.Read()){
-                System.Console.WriteLine(rdr.GetInt32(0) + " " + rdr.GetString(1)+ " " + rdr.GetInt32(2)+ " " + rdr.GetDateTime(3) + " " + rdr.GetInt32(4));
-                Driver newDriver = new Driver()
-                {
-                    ID = rdr.GetInt32(0),
-                    DriverName = rdr.GetString(1),
-                    Rating = rdr.GetInt32(2),
-                    DateHired = rdr.GetDateTime(3),
-                    Deleted = rdr.GetInt32(4)
-                };
-                drivers.Add(newDriver);
+            List<Driver> allDrivers = new List<Driver>();
+            while(rdr.Read())
+            {
+                allDrivers.Add(new Driver(){ID = rdr.GetInt32(0), DriverName = rdr.GetString(1), Rating = rdr.GetInt32(2), DateHired = rdr.GetDateTime(3), Deleted = rdr.GetInt32(4)});
             }
-            con.Close();
 
-            return drivers;
+            return allDrivers;
         }
 
         public Driver GetDriver(int id){
