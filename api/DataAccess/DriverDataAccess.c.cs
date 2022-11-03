bf8166a68752a2675112db070f5b103bcc5a1fcb
Lacy <44073703+lacyrich@users.noreply.github.com>
2022-11-03 10:58:07 -0500

    hi

--- api/DataAccess/DriverDataAccess.c.cs
+++ api/DataAccess/DriverDataAccess.c.cs
@@ -52,7 +52,18 @@ namespace mis321pa3api.api.DataAccess
             cmd.ExecuteNonQuery();
             
             rdr.Read();
-            return new Driver() {rdr.GetInt32(0) + " " + rdr.GetString(1)+ " " + rdr.GetInt32(2)+ " " + rdr.GetDateTime(3) + " " + rdr.GetInt32(4)};
+            System.Console.WriteLine(rdr.GetInt32(0) + " " + rdr.GetString(1)+ " " + rdr.GetInt32(2)+ " " + rdr.GetDateTime(3) + " " + rdr.GetInt32(4));
+                Driver newDriver = new Driver()
+                {
+                    ID = rdr.GetInt32(0),
+                    DriverName = rdr.GetString(1),
+                    Rating = rdr.GetInt32(2),
+                    DateHired = rdr.GetDateTime(3),
+                    Deleted = rdr.GetInt32(4)
+                };
+                drivers.Add(newDriver);
+                return new Driver();
+            //return new Driver() {rdr.GetInt32(0) + " " + rdr.GetString(1)+ " " + rdr.GetInt32(2)+ " " + rdr.GetDateTime(3) + " " + rdr.GetInt32(4)};
         }
     }
 
