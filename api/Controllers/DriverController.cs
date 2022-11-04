0a16733b1ba99b7b96bd77ee57149305bb606a72
Lacy <44073703+lacyrich@users.noreply.github.com>
2022-11-04 13:48:34 -0500

    hi

--- api/Controllers/DriverController.cs
+++ api/Controllers/DriverController.cs
@@ -41,10 +41,10 @@ namespace mis321pa3api.api.Controllers
 
         // PUT: api/Driver/5 //edit driver rating
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Driver value)
+        public void Put(int id, [FromBody] string value)
         {
             IUpdateDriverRating editRating = new UpdateDriverRating();
-            editRating.UpdateDriverRating(value);
+            editRating.UpdateDriverRating(id);
         }
 
         // DELETE: api/Driver/5 //fire driver
